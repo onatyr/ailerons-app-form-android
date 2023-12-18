5539d7799fdf559f33ceb4338942659a1df2cdd4
onatyr <onat.rigault@gmail.com>
2023-12-18 21:49:24 +0100

    fixed + situation field

--- app/src/main/java/fr/onat68/ailerons_app_android/screens/observationContext/ObservationContextComposables.kt
+++ app/src/main/java/fr/onat68/ailerons_app_android/screens/observationContext/ObservationContextComposables.kt
@@ -1,19 +1,29 @@
 package fr.onat68.ailerons_app_android.screens.observationContext
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -28,8 +38,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import fr.onat68.ailerons_app_android.R
 import java.util.Calendar
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -196,12 +208,64 @@ fun LocationField() {
 @Composable
 fun DepthField() {
     Text(text = "DepthField")
-    TextField(value = "hey", onValueChange = { }, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+    TextField(
+        value = "hey",
+        onValueChange = { },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
 }
 
 @Composable
 fun SituationField() {
     Text(text = "SituationField")
+    val situationList = LocalContext.current.resources.getStringArray(R.array.situation_field_array)
+    var selected by remember { mutableStateOf(situationList.first()) }
+    var expanded by remember { mutableStateOf(false) }
+    Box {
+        Column {
+            OutlinedTextField(
+                value = (selected),
+                onValueChange = { },
+                label = { Text(text = "Situation") },
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = { Icon(Icons.Outlined.ArrowDropDown, null) },
+                readOnly = true
+            )
+            DropdownMenu(
+                modifier = Modifier.fillMaxWidth(),
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+            ) {
+                situationList.forEach { entry ->
+
+                    DropdownMenuItem(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {
+                            selected = entry
+                            expanded = false
+                        },
+                        text = {
+                            Text(
+                                text = (entry),
+                                modifier = Modifier
+                                    .wrapContentWidth()
+                                    .align(Alignment.Start)
+                            )
+                        }
+                    )
+                }
+            }
+        }
+        Spacer(
+            modifier = Modifier
+                .matchParentSize()
+                .background(Color.Transparent)
+                .padding(10.dp)
+                .clickable(
+                    onClick = { expanded = !expanded }
+                )
+        )
+    }
 }
 
 //@Composable

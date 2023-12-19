a5f96094638bba4eedc46c3d7b6173ba1ef01e4f
onatyr <onat.rigault@gmail.com>
2023-12-19 14:24:53 +0100

    location flight mode + accuracy

--- app/src/main/java/fr/onat68/ailerons_app_android/screens/context/ContextViewModel.kt
+++ app/src/main/java/fr/onat68/ailerons_app_android/screens/context/ContextViewModel.kt
@@ -5,6 +5,7 @@ import android.location.Location
 import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Priority
 import fr.onat68.ailerons_app_android.FormulaireModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,10 +18,9 @@ class ContextViewModel(fusedLocationClient: FusedLocationProviderClient): ViewMo
 
     private var _location = MutableStateFlow(Location("observationLocation"))
     val location: Flow<Location> = _location
-    val requestLocation = fusedLocationClient.lastLocation.addOnSuccessListener { lastLocation : Location -> // Attention, peut faire planter dans de rare cas car nullable
+    val requestLocation = fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null) .addOnSuccessListener { lastLocation : Location -> // Attention, peut faire planter dans de rare cas car nullable
         _location.value = lastLocation
     }
-
 
     fun onTimeChange(hour: Int, min: Int){
         _newForm.value = _newForm.value.copy(hour = hour, min = min)
